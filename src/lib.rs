130486fa1e793866048917187b6dbca4875bfeb0
Steve Masterman <steve@vermonster.com>
2019-04-11 12:40:24 -0400

    wip

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,9 @@
-use md5;
 use std::process;
-
-extern crate rpassword;
-extern crate clipboard;
 
 use clipboard::ClipboardProvider;
 use clipboard::ClipboardContext;
+//use md5;
+use rpassword::read_password_from_tty;
 
 fn print_usage() {
     let usage = "Generate a unique password for each account using a unique nickname and a master password
@@ -20,7 +18,6 @@ Options:
     println!("{}", usage);
 }
 
-
 fn set_clipboard(text: &str) {
     let mut ctx: ClipboardContext = ClipboardProvider::new().unwrap();
     ctx.set_contents(text.to_owned()).expect("Error copying to clipboard.");
@@ -28,25 +25,25 @@ fn set_clipboard(text: &str) {
 }
 
 fn oplop() {
-    let nickname = rpassword::read_password_from_tty(Some("Enter account nickname: ")).unwrap();
-    let password = rpassword::read_password_from_tty(Some("Enter master password:")).unwrap();
+    let nickname = read_password_from_tty(Some("Enter account nickname: ")).unwrap();
+    let password = read_password_from_tty(Some("Enter master password:")).unwrap();
     let hash = format!("oplop{}{}", nickname, password);
     set_clipboard(&hash);
 }
 
 fn oplop_new() {
-    let nickname = rpassword::read_password_from_tty(Some("Enter account nickname: ")).unwrap();
-    let password = rpassword::read_password_from_tty(Some("Enter master password:")).unwrap();
+    let nickname = read_password_from_tty(Some("Enter account nickname: ")).unwrap();
+    let password = read_password_from_tty(Some("Enter master password:")).unwrap();
 
-    let confirm_nickname = rpassword::read_password_from_tty(Some("Confirm account nickname: ")).unwrap();
+    let confirm_nickname = read_password_from_tty(Some("Confirm account nickname: ")).unwrap();
     if !(confirm_nickname == nickname) {
         eprintln!("nicknames don't match");
         process::exit(1);
     }
 
-    let confirm_password = rpassword::read_password_from_tty(Some("Confirm master password:")).unwrap();
-    if !(confirm_nickname == nickname) {
-        eprintln!("nicknames don't match");
+    let confirm_password = read_password_from_tty(Some("Confirm master password:")).unwrap();
+    if !(confirm_password == password) {
+        eprintln!("passwords don't match");
         process::exit(1);
     }
 
