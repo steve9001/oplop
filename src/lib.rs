63539a9295e967e8ac34e65639560e36ea56d208
Steve Masterman <steve@vermonster.com>
2019-04-15 09:51:08 -0400

    cargo fmt

--- src/lib.rs
+++ src/lib.rs
@@ -5,8 +5,8 @@
 use std::process;
 
 use base64;
-use clipboard::ClipboardProvider;
 use clipboard::ClipboardContext;
+use clipboard::ClipboardProvider;
 use md5;
 use regex::Regex;
 use rpassword::read_password_from_tty;
@@ -37,16 +37,17 @@ fn oplop_password(hash: &str) -> String {
                 hash[0..8].to_owned()
             } else {
                 let prefix_len = m.end() - m.start();
-                m.as_str().to_owned() + &hash[..8-prefix_len]
+                m.as_str().to_owned() + &hash[..8 - prefix_len]
             }
         }
-        None => "1".to_owned() + &hash[..7]
+        None => "1".to_owned() + &hash[..7],
     }
 }
 
 fn set_clipboard(text: &str) {
     let mut ctx: ClipboardContext = ClipboardProvider::new().unwrap();
-    ctx.set_contents(text.to_owned()).expect("Error copying to clipboard.");
+    ctx.set_contents(text.to_owned())
+        .expect("Error copying to clipboard.");
     println!("Account password copied to clipboard!");
 }
 
@@ -92,8 +93,8 @@ pub fn run(args: &[String]) {
 #[cfg(test)]
 mod tests {
     use super::{oplop_hash, oplop_password};
-    use std::fs;
     use json;
+    use std::fs;
 
     #[test]
     fn example() {
@@ -104,14 +105,8 @@ mod tests {
             let master = &case["master"].as_str().unwrap();
             let hash = &case["hash"].as_str().unwrap();
             let password = &case["password"].as_str().unwrap();
-            assert_eq!(
-                &oplop_hash(label, master),
-                hash
-            );
-            assert_eq!(
-                &oplop_password(hash),
-                password
-            );
+            assert_eq!(&oplop_hash(label, master), hash);
+            assert_eq!(&oplop_password(hash), password);
         }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
-use std::env;
 use oplop;
+use std::env;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
